40f4d5e27e6c97014517bf9690350b567ac116be
PapiLuee <41023873+LueDev@users.noreply.github.com>
2023-11-20 13:51:59 -0500

    Updated both the EyesOnMe and Keypad components to reflect the desired output. All tests passing.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
+import React from 'react'; 
 
 function Keypad (){
+
+function onInput(text){
+    console.log("Entering password...")
+}
+
     return (
-        <div></div>
+        <div>
+            <h1>PASSWORD APPLICATION</h1>
+            <label htmlFor="pw">Enter the password: </label>
+            <input id="pw" type="password" onChange={(event) => onInput(event.target.value)}></input>
+        </div>
     )
 }
 
